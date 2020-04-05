d597e39a199bc2564bc9d0397b051e1a59ab0192
Usama Ansari <usmslm102@gmail.com>
2020-04-06 04:39:40 +0530

    completed problem : 10001st prime

---
+++ 7.js
@@ -0,0 +1,38 @@
+/*
+Problem 7: 10001st prime
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+
+let MAX_NUMBER = 1000000;
+
+let isPrime = new Array(MAX_NUMBER);
+
+let prime = [];
+
+function SieveOfEratosthenes() {
+    for (let i = 0; i < MAX_NUMBER; i++) {
+        isPrime[i] = true;
+    }
+
+    for (let i = 2; i < MAX_NUMBER; i++) {
+        if (isPrime[i] == true) {
+            for (let j = i * i; j < MAX_NUMBER; j = j + i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    for (let i = 2; i < MAX_NUMBER; i++) {
+        if (isPrime[i] == true) {
+            prime.push(i);
+        }
+    }
+
+    console.log(prime.length);
+}
+
+SieveOfEratosthenes();
+
+console.log(prime[10000]);
