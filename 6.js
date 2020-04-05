435d47967c3407210cdaaa17d63cb8ad46d1b78d
Usama Ansari <usmslm102@gmail.com>
2020-04-06 03:18:03 +0530

    solved problem : Sum square difference

---
+++ 6.js
@@ -0,0 +1,27 @@
+/*
+Problem 6: Sum square difference
+The sum of the squares of the first ten natural numbers is,
+1^2+2^2+...+10^2=385
+
+The square of the sum of the first ten natural numbers is,
+(1+2+...+10)^2=55^2=3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025−385=2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+
+let n = 100;
+
+let sumOfIndividualSqaure = 0;
+let sumOfAll = 0;
+//Find squares of indiviual number
+
+for (let i = 1; i <= n; i++) {
+    sumOfIndividualSqaure += i * i;
+    sumOfAll += i;
+}
+
+
+console.log((sumOfAll * sumOfAll) - sumOfIndividualSqaure)
